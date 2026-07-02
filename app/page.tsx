ab6670e680f0565f77c77bcdc3120f1e4a74c066
d09727418-ctrl <d09727418@gmail.com>
2026-07-02 11:22:05 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
     <main className="h-screen flex flex-col items-center justify-center bg-black text-white">
       
       <h1 className="text-5xl font-bold mb-4">
-        Dan's Digital Space
+        SD95的博客
       </h1>
 
       <p className="text-lg text-gray-400 mb-6">
-        Build. Think. Break.
+        不是你小子真叫啊
       </p>
       <Link href={"/blog"}>
        <button className="px-6 py-3 border border-white rounded-full hover:bg-white hover:text-black transition">

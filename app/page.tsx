dafae0108524a2652b21bede61c6bfdf49b84317
Mouller <d09727418@gmail.com>
2026-07-31 16:17:49 +0800

    给前端的人机验证去掉了

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
-import { useState } from "react";
 import Link from "next/link";
 export default function Home() {
-  const [show, setShow] = useState(true);
-
   return (
     <main className="h-screen flex flex-col items-center justify-center bg-black text-white">
       
@@ -19,29 +16,6 @@ export default function Home() {
         Enter
       </button>
       </Link>
-     
-
-      {show && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80">
-          <div className="bg-white text-black p-6 rounded-lg text-center">
-            <p className="mb-4">请问您是真人吗？</p>
-
-            <button
-              className="mr-2 px-4 py-2 bg-black text-white rounded"
-              onClick={() => setShow(false)}
-            >
-              我是真人
-            </button>
-
-            <button
-              className="px-4 py-2 bg-gray-300 rounded"
-              onClick={() => alert("抱歉，您不能访问该网站")}
-            >
-              不是
-            </button>
-          </div>
-        </div>
-      )}
 
     </main>
   );

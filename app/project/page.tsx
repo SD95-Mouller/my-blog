e7195a6b96b9638b149a68c858df9f4b58318dfd
Mouller <d09727418@gmail.com>
2026-07-31 18:21:45 +0800

    正常显示项目页面

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,1 +1,10 @@
-<h1>2026.7.31:站长还没个人项目。</h1>
+export default function project(){
+  return(
+    <main className="min-h-screen bg-black text-white p-10">
+      <Link href="/">
+        <p className="mb-6">返回首页</p>
+      </Link>
+      <h1 className="text-4xl font-bold mb-6">站长还没有个人项目</h1>
+    </main>
+  );
+}

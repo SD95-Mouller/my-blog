977bb5f59010ada9ecab4d9577a143160b65dea0
Mouller <d09727418@gmail.com>
2026-08-08 21:46:44 +0800

    Update page.tsx

--- app/project/Vworld/page.tsx
+++ app/project/Vworld/page.tsx
@@ -4,6 +4,7 @@ export default function Vworld(){
     <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white px-6 py-12">
       <Link href="/project">
         <p className="mb-6">⬅返回上一页</p>
+      </Link>
         <Link href={"/project/Vworld/diary"}>
        <button className="px-6 py-3 border border-white rounded-full hover:bg-white hover:text-black transition">
         开发日记

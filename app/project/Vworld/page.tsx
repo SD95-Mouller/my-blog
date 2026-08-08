acd2cad3343c6c1f4bac34154c9ed43555a2be9e
Mouller <d09727418@gmail.com>
2026-08-08 21:44:41 +0800

    Vworld首页

--- app/project/Vworld/page.tsx
+++ app/project/Vworld/page.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 export default function Vworld(){
     return(
-        <main className="h-screen flex flex-col items-center justify-center bg-black text-white">
-        
+    <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white px-6 py-12">
+      <Link href="/project">
+        <p className="mb-6">⬅返回上一页</p>
         <Link href={"/project/Vworld/diary"}>
        <button className="px-6 py-3 border border-white rounded-full hover:bg-white hover:text-black transition">
         开发日记
